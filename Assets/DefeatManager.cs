179517f36bbcc6841da17cb6db117be48e68949a
Orion Leclerc <orion.leclerc@gmail.com>
2022-12-09 13:16:25 +0100

    Ajout defeat scene

--- Assets/DefeatManager.cs
+++ Assets/DefeatManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DefeatManager : MonoBehaviour
 {
@@ -20,7 +21,7 @@ public class DefeatManager : MonoBehaviour
     {
         if (checkDefeat())
         {
-            Debug.Log("Perdu"); 
+            SceneManager.LoadScene("DefeatScreen");
         }
     }
 
