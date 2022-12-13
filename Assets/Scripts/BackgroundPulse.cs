05fa1a59835ef47e27dd0fa5620dd99f9e2b011c
Orion Leclerc <orion.leclerc@gmail.com>
2022-12-13 17:13:56 +0100

    background elements pulse

--- Assets/Scripts/BackgroundPulse.cs
+++ Assets/Scripts/BackgroundPulse.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class BackgroundPulse : MonoBehaviour
 {
     public Conductor myCond;
+    public float minScale;
+    public float maxScale;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+            transform.localScale = Vector3.one * Mathf.Lerp(maxScale, minScale, myCond.songPositionInBeats % 1); 
     }
 }
