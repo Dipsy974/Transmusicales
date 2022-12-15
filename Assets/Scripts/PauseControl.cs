012fca15729d6ff4ec91dedb8896301af3eebb21
RENNES\o.leclerc <orion.leclerc@gmail.com>
2022-12-15 12:11:47 +0100

    menu pause fini

--- Assets/Scripts/PauseControl.cs
+++ Assets/Scripts/PauseControl.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class PauseControl : MonoBehaviour
 {
     public static bool gameIsPaused;
+    public UIMenu uiMenu;
     public Conductor myCond;
+    public GameObject layer;
+    public Blur blur;
     void Update()
     {
 
@@ -19,12 +22,17 @@ public class PauseControl : MonoBehaviour
             Time.timeScale = 0f;
             myCond.music.Pause();
             myCond.dspSongTime = (float)AudioSettings.dspTime;
+            uiMenu.ShowOptions(layer);
+            blur.enabled = true;
         }
         else
         {
             Time.timeScale = 1;
             myCond.music.Play();
             myCond.dspSongTime = (float)AudioSettings.dspTime - myCond.songPosition;
+            uiMenu.HideOptions(layer);
+            blur.enabled = false;
         }
     }
+
 }
